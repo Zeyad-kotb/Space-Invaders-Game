d4d15473d0b6502906cba7b1903896c4a4ea2c49
Zeyad-kotb <zeyadkotb2007@gmail.com>
2026-05-03 09:25:03 +0300

    Added Project outline

---
+++ spaceinvaders by sirius/spaceinvaders by sirius/game.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "spaceship.hpp"
+#include "obstacle.hpp"
+#include "alien.hpp"
+#include "mysteryship.hpp"
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void Draw();
+	void Update();
+	void HandleInput();
+	bool run;
+	int lives;
+	int score;
+	int highscore;
+private:
+	void DeleteInactiveLasers();
+	std::vector<Obstacle> CreateObstacles();
+	std::vector<Alien> CreatAliens();
+	void MoveAliens();
+	void MoveDownAkiens(int distance);
+	void AlienShootLaser();
+	void CheckForCollisions();
+	void GameOver();
+	void Reset();
+	void InitGame();
+	void checkForHighscore();
+	void saveHighscoreToFile(int highscore);
+	int loadHighscoreFromFile();
+	Spaceship spaceship;
+	std::vector<Obstacle> obstacles;
+	std::vector<Alien> aliens;
+	int aliensDirection;
+	std::vector<Laser> alienLasers;
+	constexpr static float alienLaserShootInterval = 0.35;
+	float timeLastAlienFired;
+	MysteryShip mysteryship;
+	float mysteryShipSpawnInterval;
+	float timeLastSpawn;
+};
